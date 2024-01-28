e04ce36311a25a188a8d96857c2d1b21a266d429
harin <gkflsgg@kyonggi.ac.kr>
2024-01-29 04:38:22 +0900

    css: tailwind input, button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import ProjectSideBar from "./components/ProjectsSideBar";
+import NewProject from "./components/NewProject.jsx";
+import ProjectSideBar from "./components/ProjectsSideBar.jsx";
 
 function App() {
   return (
-    <main className="h-screen my-8">
+    <main className="h-screen my-8 flex gap-8">
       <ProjectSideBar />
+      <NewProject />
     </main>
   );
 }
