0b6afdf209fb15d8da12599b61be9fe07757eaaf
harin <gkflsgg@kyonggi.ac.kr>
2024-01-29 04:12:44 +0900

    first commit

---
+++ src/components/ProjectsSideBar.jsx
@@ -0,0 +1,13 @@
+export default function ProjectSideBar(){
+    return(<aside>
+        <h2>
+            Your Projects
+            <div>
+                <button>
+                    + Add Project
+                </button>
+            </div>
+        </h2>
+    </aside>
+    );
+}
