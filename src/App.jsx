94885fbed03a12a77efb9e4e8ee216c654088670
harin <gkflsgg@kyonggi.ac.kr>
2024-01-29 04:56:28 +0900

    separate component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import NewProject from "./components/NewProject.jsx";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
 import ProjectSideBar from "./components/ProjectsSideBar.jsx";
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectSideBar />
-      <NewProject />
+      <NoProjectSelected />
     </main>
   );
 }
