e9d8f0552d7c5da08a10df41491576ddd5e5f9c1
harin <gkflsgg@kyonggi.ac.kr>
2024-01-29 05:44:22 +0900

    Input Validation & Error Modal

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,26 +1,42 @@
 import { useRef } from "react";
 import Input from "./Input.jsx";
+import Modal from "./Modal.jsx";
 
-export default function NewProject({onAdd}) {
+export default function NewProject({ onAdd }) {
+  const modal = useRef();
+
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
 
-  function handleSave(){
+  function handleSave() {
     const enteredTitle = title.current.value;
     const enteredDescription = description.current.value;
     const enteredDueDate = dueDate.current.value;
 
     //validation...
-    onAdd({
-      title: enteredTitle,
-      description: enteredDescription,
-      dueDate: enteredDueDate
-    });
-
+    if (
+      enteredTitle.trim() === "" ||
+      enteredDescription.trim() === "" ||
+      enteredDueDate.trim() === ""
+    ){
+      modal.current.open();
+      return;
+    }
+      onAdd({
+        title: enteredTitle,
+        description: enteredDescription,
+        dueDate: enteredDueDate,
+      });
   }
 
   return (
+    <>
+    <Modal ref={modal} buttonCaption="Close">
+      <h2>Invalid Input</h2>
+      <p>Oops... looks like tou forgot to enter a value.</p>
+      <p>Please make sure you provide a valid value for every input field.</p>
+    </Modal>
     <div className="w-[32rem] mt-16">
       <menu className="flex items-center justify-end gap-4 my-4">
         <li>
@@ -29,9 +45,10 @@ export default function NewProject({onAdd}) {
           </button>
         </li>
         <li>
-          <button 
-          className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
-          onClick={handleSave}>
+          <button
+            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+            onClick={handleSave}
+          >
             Save
           </button>
         </li>
@@ -42,5 +59,6 @@ export default function NewProject({onAdd}) {
         <Input type="date" ref={dueDate} label="Due Date" />
       </div>
     </div>
+    </>
   );
 }
